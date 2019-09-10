227e90eb31264e7d878e46b8367d3392d6a2f6bb
SunshineFett <sunshineFett@gmail.com>
2019-09-10 06:31:17 -0400

    smaller functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const cart = [10, 5, 15];
 
+const SHIPPING_COST = 10;
+
 const fakeAPICharge = total => true;
 const fakeSendRecipt = total => true;
 
+const getSubTotal = cart => cart.reduce((tempTotal, item) => tempTotal + item);
+
+const getTotal = subTotal => subTotal + SHIPPING_COST;
+
+const sendRecipt = (email, total) =>
+  fakeSendRecipt({
+    email,
+    total
+  });
+
 const checkout = cart => {
-  let total = cart.reduce((tempTotal, item) => tempTotal + item);
-  total = total + 10;
+  const subtotal = getSubTotal(cart);
+  const total = getTotal(subtotal);
   const orderSuccess = fakeAPICharge(total);
   if (orderSuccess) {
-    fakeSendRecipt({
+    sendRecipt({
       email: "fakeemail@gmail.com",
       total
     });
